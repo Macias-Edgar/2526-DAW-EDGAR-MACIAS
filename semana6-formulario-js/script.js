45db3bd4e348a01f5b7915b5a63d3e5fb3bb2c0e
Macias farias Edgar Fernando <ef.maciasf@uea.edu.ec>
2026-01-15 17:22:45 -0500

    Comint

---
+++ semana6-formulario-js/script.js
@@ -0,0 +1,42 @@
+body {
+  font-family: Arial, sans-serif;
+  background: #f2f2f2;
+  padding: 30px;
+}
+
+form {
+  background: white;
+  padding: 20px;
+  max-width: 400px;
+  margin: auto;
+  border-radius: 8px;
+}
+
+label {
+  display: block;
+  margin-top: 10px;
+}
+
+input {
+  width: 100%;
+  padding: 8px;
+  margin-top: 3px;
+}
+
+input.valido {
+  border: 2px solid green;
+}
+
+input.invalido {
+  border: 2px solid red;
+}
+
+.error {
+  color: red;
+  font-size: 12px;
+}
+
+button {
+  margin-top: 15px;
+  padding: 8px;
+}
